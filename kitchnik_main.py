78890b56b7042be54fe58a8a1a6b638d62c5603e
alemosie <alexandra.siega@gmail.com>
2016-12-18 16:36:19 -0500

    add dynamic functionality to /hello and /hello/<name> routes

--- kitchnik_main.py
+++ kitchnik_main.py
@@ -1,12 +1,17 @@
 import logging
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, world!'
+def index():
+    return "You've found the index page!"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 @app.errorhandler(500)
 def server_error(e):
